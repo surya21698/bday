1b4117650b760f6d084ddc221a9d5e7fd4c3d8da
surya21698 <58200844+surya21698@users.noreply.github.com>
2025-04-02 22:25:20 +0530

    Update effect.js

--- effect.js
+++ effect.js
@@ -16,10 +16,14 @@ $('document').ready(function(){
 			$('#b77').animate({top:240, left: vw+250},500);
 		});
 
-	$('#turn_on').click(function(){
-		var audio = $('.song')[0];
-        audio.play();
-		$('#bulb_yellow').addClass('bulb-glow-yellow');
+
+
+
+   // Function that simulates the button click behavior
+    function triggerAction() {
+      var audio = $('.song')[0];
+      audio.play();
+      $('#bulb_yellow').addClass('bulb-glow-yellow');
 		$('#bulb_red').addClass('bulb-glow-red');
 		$('#bulb_blue').addClass('bulb-glow-blue');
 		$('#bulb_green').addClass('bulb-glow-green');
@@ -32,30 +36,95 @@ $('document').ready(function(){
 		$('#bulb_pink').addClass('bulb-glow-pink-after');
 		$('#bulb_orange').addClass('bulb-glow-orange-after');
 		$('body').addClass('peach');
-		$(this).fadeOut('slow').delay(3000).promise().done(function(){
-			$('#bannar_coming').fadeIn('slow');
-		});
-	});
-	$('#play').click(function(){
-	
-        
-		$('body').css('backgroud-color','#FFF');
-		$('body').addClass('peach-after');
-		$(this).fadeOut('slow').delay(6000).promise().done(function(){
-			$('#bannar_coming').fadeIn('slow');
-		});
-	});
-
-	$('#bannar_coming').click(function(){
+		
 		$('.bannar').addClass('bannar-come');
 		$('body').css('backgroud-color','#FFF');
 		$('body').addClass('peach-after');
-		$(this).fadeOut('slow').delay(6000).promise().done(function(){
-			$('#balloons_flying').fadeIn('slow');
-			
-		});
-	});
+		
+		
+		$('.balloon-border').animate({top:-500},8000);
+		$('#b1,#b4,#b5,#b7').addClass('balloons-rotate-behaviour-one');
+		$('#b2,#b3,#b6').addClass('balloons-rotate-behaviour-two');
+		// $('#b3').addClass('balloons-rotate-behaviour-two');
+		// $('#b4').addClass('balloons-rotate-behaviour-one');
+		// $('#b5').addClass('balloons-rotate-behaviour-one');
+		// $('#b6').addClass('balloons-rotate-behaviour-two');
+		// $('#b7').addClass('balloons-rotate-behaviour-one');
+		loopOne();
+		loopTwo();
+		loopThree();
+		loopFour();
+		loopFive();
+		loopSix();
+		loopSeven();
+		setTimeout(function() {
+    document.getElementById('swipeArea').style.opacity = '0';
+}, 1);
 
+		
+		
+    }
+
+    // Swipeable Area Detection
+    const swipeArea = document.getElementById('swipeArea');
+    let touchStartX = 0;
+    let touchStartY = 0;
+    let swipeDetected = false;
+    let isTouching = false; // Track if the user is holding down to swipe
+
+    // Detect when the user clicks/touches the area (start)
+    swipeArea.addEventListener('touchstart', function (e) {
+      isTouching = true;
+      const touch = e.touches[0];
+      touchStartX = touch.clientX;
+      touchStartY = touch.clientY;
+      swipeDetected = false;
+    });
+
+    swipeArea.addEventListener('mousedown', function (e) {
+      isTouching = true;
+      touchStartX = e.clientX;
+      touchStartY = e.clientY;
+      swipeDetected = false;
+    });
+
+    // Detect when the user moves (swipes)
+    swipeArea.addEventListener('touchmove', function (e) {
+      if (!isTouching) return;
+      const touch = e.touches[0];
+      const deltaX = touch.clientX - touchStartX;
+      const deltaY = touch.clientY - touchStartY;
+
+      // If swipe distance is greater than a threshold, trigger the action
+      if (!swipeDetected && (Math.abs(deltaX) > 30 || Math.abs(deltaY) > 30)) {
+        swipeDetected = true;
+        triggerAction();  // Trigger the action similar to button click
+      }
+    });
+
+    swipeArea.addEventListener('mousemove', function (e) {
+      if (!isTouching) return;
+      const deltaX = e.clientX - touchStartX;
+      const deltaY = e.clientY - touchStartY;
+
+      // If swipe distance is greater than a threshold, trigger the action
+      if (!swipeDetected && (Math.abs(deltaX) > 30 || Math.abs(deltaY) > 30)) {
+        swipeDetected = true;
+        triggerAction();  // Trigger the action similar to button click
+      }
+    });
+
+    // Detect when the user releases touch or mouse (end)
+    swipeArea.addEventListener('touchend', function () {
+      isTouching = false;
+    });
+
+    swipeArea.addEventListener('mouseup', function () {
+      isTouching = false;
+    });
+
+	
+	
 	function loopOne() {
 		var randleft = 1000*Math.random();
 		var randtop = 500*Math.random();
@@ -107,97 +176,6 @@ $('document').ready(function(){
 		});
 	}
 
-	$('#balloons_flying').click(function(){
-		$('.balloon-border').animate({top:-500},8000);
-		$('#b1,#b4,#b5,#b7').addClass('balloons-rotate-behaviour-one');
-		$('#b2,#b3,#b6').addClass('balloons-rotate-behaviour-two');
-		// $('#b3').addClass('balloons-rotate-behaviour-two');
-		// $('#b4').addClass('balloons-rotate-behaviour-one');
-		// $('#b5').addClass('balloons-rotate-behaviour-one');
-		// $('#b6').addClass('balloons-rotate-behaviour-two');
-		// $('#b7').addClass('balloons-rotate-behaviour-one');
-		loopOne();
-		loopTwo();
-		loopThree();
-		loopFour();
-		loopFive();
-		loopSix();
-		loopSeven();
-		
-		$(this).fadeOut('slow').delay(5000).promise().done(function(){
-			$('#cake_fadein').fadeIn('slow');
-		});
-	});	
-
-	$('#cake_fadein').click(function(){
-		$('.cake').fadeIn('slow');
-		$(this).fadeOut('slow').delay(3000).promise().done(function(){
-			$('#light_candle').fadeIn('slow');
-		});
-	});
-
-	$('#light_candle').click(function(){
-		$('.fuego').fadeIn('slow');
-		$(this).fadeOut('slow').promise().done(function(){
-			$('#wish_message').fadeIn('slow');
-		});
-	});
-
-		
-	$('#wish_message').click(function(){
-		 vw = $(window).width()/2;
-
-		$('#b1,#b2,#b3,#b4,#b5,#b6,#b7').stop();
-		$('#b1').attr('id','b11');
-		$('#b2').attr('id','b22')
-		$('#b3').attr('id','b33')
-		$('#b4').attr('id','b44')
-		$('#b5').attr('id','b55')
-		$('#b6').attr('id','b66')
-		$('#b7').attr('id','b77')
-		$('#b11').animate({top:240, left: vw-350},500);
-		$('#b22').animate({top:240, left: vw-250},500);
-		$('#b33').animate({top:240, left: vw-150},500);
-		$('#b44').animate({top:240, left: vw-50},500);
-		$('#b55').animate({top:240, left: vw+50},500);
-		$('#b66').animate({top:240, left: vw+150},500);
-		$('#b77').animate({top:240, left: vw+250},500);
-		$('.balloons').css('opacity','0.9');
-		$('.balloons h2').fadeIn(3000);
-		$(this).fadeOut('slow').delay(3000).promise().done(function(){
-			$('#story').fadeIn('slow');
-		});
-	});
-	
-	$('#story').click(function(){
-		$(this).fadeOut('slow');
-		$('.cake').fadeOut('fast').promise().done(function(){
-			$('.message').fadeIn('slow');
-		});
-		
-		var i;
-
-		function msgLoop (i) {
-			$("p:nth-child("+i+")").fadeOut('slow').delay(800).promise().done(function(){
-			i=i+1;
-			$("p:nth-child("+i+")").fadeIn('slow').delay(1000);
-			if(i==50){
-				$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
-					$('.cake').fadeIn('fast');
-				});
-				
-			}
-			else{
-				msgLoop(i);
-			}			
-
-		});
-			// body...
-		}
-		
-		msgLoop(0);
-		
-	});
 });
 
 
