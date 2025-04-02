d94a91bf038fba9f731d1c967ea8277a4526b397
surya21698 <58200844+surya21698@users.noreply.github.com>
2025-04-02 22:50:25 +0530

    Update effect.js

--- effect.js
+++ effect.js
@@ -7,13 +7,13 @@ $('document').ready(function(){
 		$(window).resize(function(){
 			 vw = $(window).width()/2;
 			$('#b1,#b2,#b3,#b4,#b5,#b6,#b7').stop();
-			$('#b11').animate({top:240, left: vw-350},500);
-			$('#b22').animate({top:240, left: vw-250},500);
-			$('#b33').animate({top:240, left: vw-150},500);
-			$('#b44').animate({top:240, left: vw-50},500);
-			$('#b55').animate({top:240, left: vw+50},500);
-			$('#b66').animate({top:240, left: vw+150},500);
-			$('#b77').animate({top:240, left: vw+250},500);
+			$('#b11').animate({top: '30vh', left: `calc(50vw - 350px)`}, 500);
+			$('#b22').animate({top: '30vh', left: `calc(50vw - 350px)`}, 500);
+			$('#b33').animate({top: '30vh', left: `calc(50vw - 350px)`}, 500);
+			$('#b44').animate({top: '30vh', left: `calc(50vw - 350px)`}, 500);
+			$('#b55').animate({top: '30vh', left: `calc(50vw - 350px)`}, 500);
+			$('#b66').animate({top: '30vh', left: `calc(50vw - 350px)`}, 500);
+			$('#b77').animate({top: '30vh', left: `calc(50vw - 350px)`}, 500);
 		});
 
 
@@ -21,6 +21,8 @@ $('document').ready(function(){
 
    // Function that simulates the button click behavior
     function triggerAction() {
+
+		
       var audio = $('.song')[0];
       audio.play();
       $('#bulb_yellow').addClass('bulb-glow-yellow');
